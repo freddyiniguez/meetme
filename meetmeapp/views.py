20a21a2ead0e45c608e689e93db9ac0d957a35a6
Freddy Iñiguez López <iniguez.devt@gmail.com>
2016-05-24 09:54:27 -0500

    Retrieving the meetings to the homepage.

--- meetmeapp/views.py
+++ meetmeapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Meeting
 
 def meeting_list(request):
-	return render(request, 'meetmeapp/meeting_list.html', {})
+	meetings = Meeting.objects.filter(start_date__lte=timezone.now()).order_by('start_date')
+	return render(request, 'meetmeapp/meeting_list.html', {'meetings':meetings})
